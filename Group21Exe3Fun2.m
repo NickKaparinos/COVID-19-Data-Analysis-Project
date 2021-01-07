042b7ab50c41d31c8605b9e60ec077de3aca01c3
Vasiliki_Zarkadoula <65190697+zrk-98@users.noreply.github.com>
2021-01-07 11:47:04 +0200

    Update Group21Exe3Fun2.m

--- Group21Exe3Fun2.m
+++ Group21Exe3Fun2.m
@@ -1,11 +1,13 @@
 function [meanCI,bootMeanCI] = Group21Exe3Fun2(timeInterval,t0,B,alpha)
+% This function calculates and returns the parametric and bootstrap 
+% confidence intervals
 
-% 95% parametric CI of time intervals
+% 95% parametric CI of time intervals and hypothesis testing
 [h,~,meanCI,~] = ttest(timeInterval,t0);
 if h==0
-    fprintf('Time between peak of daily cases and deaths can be %i days (Parametric check)\n',t0);
+    fprintf('Time between peak of daily cases and deaths can be %i days (Parametric hypothesis test)\n',t0);
 else
-    fprintf('Time between peak of daily cases and deaths can not be %i days (Parametric check)\n',t0);
+    fprintf('Time between peak of daily cases and deaths can not be %i days (Parametric hypothesis test)\n',t0);
 end
 
 % 95% bootstrap CI of time intervals
@@ -16,7 +18,7 @@ limits = [lowerLim upperLim]/B*100;
 bootMeanCI = prctile(bootstrMean,limits); 
 % hypothesis testing
 if t0<bootMeanCI(2) && t0>bootMeanCI(1)
-    fprintf('Time between peak of daily cases and deaths can be %i days (Bootstrap CI check)\n',t0);
+    fprintf('Time between peak of daily cases and deaths can be %i days (Bootstrap hypothesis test)\n',t0);
 else
-    fprintf('Time between peak of daily cases and deaths can not be %i days (Bootstrap CI check)\n',t0);
+    fprintf('Time between peak of daily cases and deaths can not be %i days (Bootstrap hypothesis test)\n',t0);
 end
